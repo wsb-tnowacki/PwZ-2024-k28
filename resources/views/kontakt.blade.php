a60297bdca8831ac9813632aa200d5b6385452ef
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-09-29 17:31:03 +0200

    routing widoki szablon foreach php

---
+++ resources/views/kontakt.blade.php
@@ -0,0 +1,12 @@
+@extends('szablon')
+@section('tytul')
+    Kontakt
+@endsection
+@section('podtytul')
+Strona kontaktowa
+@endsection
+@section('tresc')
+    <div>
+        Treść strony kontaktowej Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed tempora iusto, eaque laborum recusandae aliquid ipsam numquam totam explicabo. Illo dolore natus aut dolor ab perspiciatis voluptatum. Corrupti, expedita rem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt alias ipsa nisi quaerat ut facere accusamus aperiam quas, labore veritatis eius omnis quos sint praesentium quasi optio eum similique harum!
+    </div>
+@endsection
---
+++ resources/views/test/onas.blade.php
@@ -0,0 +1,17 @@
+@extends('szablon')
+@section('tytul')
+    O nas
+@endsection
+@section('podtytul')
+Strona o nas
+@endsection
+@section('tresc')
+    <div>
+        Treść strony o nas Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed tempora iusto, eaque laborum recusandae aliquid ipsam numquam totam explicabo. Illo dolore natus aut dolor ab perspiciatis voluptatum. Corrupti, expedita rem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt alias ipsa nisi quaerat ut facere accusamus aperiam quas, labore veritatis eius omnis quos sint praesentium quasi optio eum similique harum!
+    </div>
+    <ol>
+        <?php foreach ($zadania ?? '' as $zadanie) : ?>
+        <li><?= $zadanie ?></li>
+        <?php endforeach; ?>
+    </ol>
+@endsection
